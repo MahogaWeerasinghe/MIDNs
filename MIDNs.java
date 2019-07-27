190217ed30d9c062d72839424d5f721c5dd44c98
dilini1995s <dilinitharanga721@gmail.com>
2019-07-26 23:08:44 -0700

    first commit

--- MIDNs.java
+++ MIDNs.java
@@ -1,4 +1,81 @@
-Class MIDNs{
+
+import robocode.*;
+import static robocode.util.Utils.normalRelativeAngleDegrees;
+//import java.awt.Color;
+
+// API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
+
+/**
+ * MIDNs - a robot by (your name here)
+ */
+public class MIDNs extends AlphaBot
+{
+	int dist = 50;
+	/**
+	 * run: MIDNs's default behavior
+	 */
+	public void run() {
+		// Initialization of the robot should be put here
+
+		// After trying out your robot, try uncommenting the import at the top,
+		// and the next line:
+
+		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+
+		// Robot main loop
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
+			ahead(100);
+			turnGunRight(360);
+			back(100);
+			turnGunRight(360);
+		}
+	}
+
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		//fire(1);
+			if (e.getDistance() < 50 && getEnergy() > 50) {
+			fire(3);
+		} // otherwise, fire 1.
+		else {
+			fire(1);
+		}
+		// Call scan again, before we turn the gun
+		scan();
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Replace the next line with any behavior you would like
+		//back(10);
+		//turnLeft(90 - e.getBearing());
+		turnRight(normalRelativeAngleDegrees(90 - (getHeading() - e.getHeading())));
+
+		ahead(dist);
+		dist *= -1;
+		scan();
+	}
+	public void onHitRobot(HitRobotEvent e) {
+		double turnGunAmt = normalRelativeAngleDegrees(e.getBearing() + getHeading() - getGunHeading());
+
+		turnGunRight(turnGunAmt);
+		fire(1);
+	}
 	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like
+		back(20);
+
+		
 	
+	}	
 }
